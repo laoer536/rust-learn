88b5768814ba1fa129f6f5dcd0f319e5156d09c0
LiuJie <1547698569@qq.com>
2025-03-31 17:24:36 +0800

    feat: concurrency channel finish

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -11,6 +11,7 @@
 //死锁，两个线程彼此等待对方使用完所持有的资源，线程无法继续
 //只在某些情况下发生Bug,很难可靠的复制现象和修复
 
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
@@ -18,6 +19,7 @@ use std::time::Duration;
 //通过调用的API来创建线程：1:1模型 --- 需要较小的运行时
 //语言自己实现的线程（绿色线程）：M:N模型 --- 需要更大的运行时
 //Rust: 需要权衡运行时的支持 Rust标准库仅提供1:1模型的线程 （因为有底层功能 所以有第三方包可支持M:N模式）
+
 fn main() {
     //通过thread::spawn创建新的线程
     let handle = thread::spawn(|| {
@@ -32,6 +34,8 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    channel_demo3();
 }
 
 fn demo2() {
@@ -44,4 +48,75 @@ fn demo2() {
         println!("Here's a vector: {:?}", v);
     });
     handle.join().unwrap();
+}
+
+//使用消息传递的方式来实现安全的并发 Channel
+//使用mpsc::channel函数来创建Channel
+//mpsc表示 multiple producer, single consumer(多个生产者、一个消费者)
+//返回一个tuple(元组)：里面元素分别是发送端、接收端
+fn channel_demo() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        //必须获取所有权
+        let val = String::from("hi");
+        tx.send(val).unwrap(); //send发送消息
+        // println!("val is: {}", val); //Error send获取了val的所有权（move了） 不能再使用了
+    });
+    let received = rx.recv().unwrap(); //recv会一直阻塞当前线程 直到接收到消息（返回Result<T,E>）,如果发送端关闭，就会收到一个错误
+    //扩展：另外有try_recv方法：不会阻塞，立即返回Result<T,E>:有数据达到返回Ok,里面包含着数据 否则返回错误 通常会使用循环来检查try_recv的结果
+    println!("Got: {}", received);
+}
+
+//发送多个值 看到接收端在等待的demo
+fn channel_demo2() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(200));
+        }
+    });
+    for received in rx {
+        println!("Got: {}", received); //会每隔200毫秒打印
+    }
+}
+
+//通过多个克隆创建多个发送者
+fn channel_demo3() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("1: hi"),
+            String::from("1: from"),
+            String::from("1: the"),
+            String::from("1: thread"),
+        ];
+        for val in vals {
+            tx1.send(val).unwrap(); //注意这里时克隆出来的发送端
+            thread::sleep(Duration::from_millis(200));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(200));
+        }
+    });
+    for received in rx {
+        println!("Got: {}", received); //结果是 tx和tx1发送的内容交替出现
+    }
 }
